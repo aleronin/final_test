349cf0c68799c490ab2b28d0b12b2acc7f1b69c2
aleronin <aleksey.eronin96@gmail.com>
2021-09-25 04:36:43 +0300

    template added

--- CI:CD/test_app_pipe.sh
+++ CI:CD/test_app_pipe.sh
@@ -2,18 +2,22 @@
 
 set -e
 
-
+## clone application repository
 git clone https://github.com/aleronin/final_test my_test_1
 
 cd my_test_1/app/
 
+## check of errors in code
 pylint app.py --errors-only
 if [[ $? > 0 ]]; then
   echo "Pylint found an error"
   exit 5
 fi
+
+## build of a new image
 docker build -t aleronin96/picture_test:1.1 .
 
+## push new-created image to DockerHub
 docker push aleronin96/picture_test:1.1
 
 docker-compose up -d
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 ### This is a final task
+COMPLETE DESCRIPTION
